8fcfcfdfdddbe4a16fcb38a3e30bc5c067589fc6
autopp <autopp.inc@gmail.com>
2017-06-21 09:53:50 +0900

    add imports.go

---
+++ imports.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// This code can be written also:
+//
+// import "fmt"
+// import "math"
+//
+// But, the previous style (factored import statement) is recommended.
+
+func main() {
+	fmt.Printf("Now you have %g problems.", math.Sqrt(7))
+}
