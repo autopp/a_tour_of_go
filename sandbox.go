ddc72d121588e06d08c6d4b17e5abeef10a70bd7
autopp <autopp.inc@gmail.com>
2017-06-21 09:45:44 +0900

    add sandbox.go

---
+++ sandbox.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now()) // On Go playground, this is alway "2009-11-10 23:00:00 UTC"
+}
