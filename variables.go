c3ff2aca2bc0f7101043a2f0c390115b6e23b171
autopp <autopp.inc@gmail.com>
2017-06-22 09:47:20 +0900

    add variables.go

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// A variable is declared by var statement
+
+// var statement is available in package section
+var c, python, java bool
+
+func main() {
+  // ...or in function body
+	var i int
+	fmt.Println(i, c, python, java)
+}
