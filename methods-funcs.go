c83918a0d139a006c912ebb8cce8101a5a49fb1a
autopp <autopp.inc@gmail.com>
2017-07-06 09:53:05 +0900

    add methods-funcs.go

---
+++ methods-funcs.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+  // Method is a function
+  // So, it is invoked via explicitly passing receiver argument
+	fmt.Println(Abs(v))
+}
